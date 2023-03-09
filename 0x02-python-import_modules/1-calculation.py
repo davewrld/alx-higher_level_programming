aabd7297fa602a7607b61ed1437f56781597a82f
David Mutuku <keithdave23@gmail.com>
2023-03-09 09:22:13 -0800

    calculator module

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+import calculator_1
+a = 10
+b = 5
+addition = add(a,b)
+print("{} + {} = {}".format(a,b,addition))
+subtract = sub(a,b)
+print("{} + {} = {}".format(a,b,subtract))
+multiply = mul(a,b)
+print("{} + {} = {}".format(a,b,multiply)
+division = div(a,b)
+print("{} + {} = {}".format(a,b,div))
